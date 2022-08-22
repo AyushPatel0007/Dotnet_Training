0085c045dffc1d0bb407f02bd581b005580f384e
ayush.patel <ayush.patel@DEL1-LHP-N82207>
2022-08-22 16:59:55 +0530

    push

---
+++ 20 August/MvcDemo/MvcDemo/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Index1(string Name,string s)
+        {
+            return "welcome" + Name+" <br>"+ s;
+        }
+        public String Connect(int a,int b)
+        {
+            return "welcome "+(a + b);
+        }
+
+
+    }
+}
